58f67cb57195c2bafc73c6cc50fb7366b33682e2
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2022-05-16 14:31:54 -0400

    Rebase from 'upstream'

--- husky_base/launch/base_launch.py
+++ husky_base/launch/base_launch.py
@@ -1,9 +1,9 @@
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
-import xacro
 
 
 def generate_launch_description():
@@ -56,15 +56,34 @@ def generate_launch_description():
 
     spawn_husky_velocity_controller = Node(
         package="controller_manager",
-        executable="spawner",
+        executable="spawner.py",
         arguments=["husky_velocity_controller"],
         output="screen",
     )
+
+    # Launch husky_control/control_launch.py which is just robot_localization.
+    launch_husky_control = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(PathJoinSubstitution(
+        [FindPackageShare("husky_control"), 'launch', 'control_launch.py'])))
+
+    # Launch husky_control/teleop_base_launch.py which is various ways to tele-op
+    # the robot but does not include the joystick. Also, has a twist mux.
+    launch_husky_teleop_base = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(PathJoinSubstitution(
+        [FindPackageShare("husky_control"), 'launch', 'teleop_base_launch.py'])))
+
+    # Launch husky_control/teleop_joy_launch.py which is tele-operation using a physical joystick.
+    launch_husky_teleop_joy = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(PathJoinSubstitution(
+        [FindPackageShare("husky_control"), 'launch', 'teleop_joy_launch.py'])))
 
     ld = LaunchDescription()
     ld.add_action(node_robot_state_publisher)
     ld.add_action(node_controller_manager)
     ld.add_action(spawn_controller)
     ld.add_action(spawn_husky_velocity_controller)
+    ld.add_action(launch_husky_control)
+    ld.add_action(launch_husky_teleop_base)
+    ld.add_action(launch_husky_teleop_joy)
 
     return ld
